7fbaeaf998b035762e37f62f3f463201fb013f1d
usemartier <felipemercier@hotmail.com>
2025-05-16 13:56:38 -0300

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -89,4 +90,5 @@ def consulta_por_pedido():
         return jsonify({"erro": "Erro interno", "detalhes": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
